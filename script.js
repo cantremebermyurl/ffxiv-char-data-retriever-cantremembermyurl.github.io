fb1d3ddee0f1baed3278bcf32e2a6301fff2ad6c
Alex Hua <alexhua97@gmail.com>
2019-03-28 17:41:12 -0700

    removed everything added weather api

---
+++ script.js
@@ -0,0 +1,16 @@
+var city = "Alameda";
+$.getJSON(
+  "http://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&APPID=2df45a75e1b2ded6721a5c074a1142c4&units=imperial",
+  function(data){
+    console.log(data);
+
+    var icon =
+      "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    var temp = data.main.temp;
+    var weather = data.weather[0].main + " (" + data.weather[0].description + ")";
+
+    $(".icon").attr("src", icon);
+    $(".weather").append(weather);
+    $(".temp").append(temp)
+  }
+);
