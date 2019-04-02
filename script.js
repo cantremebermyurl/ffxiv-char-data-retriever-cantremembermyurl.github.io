c22a09825e62ed0ee30c9edf86190fb8eb19680b
Alex Hua <49003854+cantremebermyurl@users.noreply.github.com>
2019-04-01 21:06:39 -0700

    Test branch (#4)
    * Added try catch for failed char search
    
    * Added server dropdown list that is auto populated via /servers api. If no server is selected in the list it defaults to searching character name across all servers.
    
    * moved weather api call into its own function
    
    set up for adding search function to weather later on if I ever decide to add that.

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 //openweathermap api
-var city = "Alameda";
-$.getJSON(
-  "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&APPID=2df45a75e1b2ded6721a5c074a1142c4&units=imperial",
-  function(data){
-    /*console.log(data);*/
+//var city = "Alameda";
+function getWeather(city){
+  $.getJSON(
+    "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&APPID=2df45a75e1b2ded6721a5c074a1142c4&units=imperial",
+    function(data){
+      /*console.log(data);*/
 
-    var icon =
-      "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-    var temp = data.main.temp;
-    var weather = data.weather[0].main + " (" + data.weather[0].description + ")";
+      var icon =
+        "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+      var temp = data.main.temp;
+      var weather = data.weather[0].main + " (" + data.weather[0].description + ")";
 
-    $(".icon").attr("src", icon);
-    $(".weather").append(weather);
-    $(".temp").append(temp)
-  }
-);
-
+      $(".icon").attr("src", icon);
+      $(".weather").append(weather);
+      $(".temp").append(temp)
+    }
+  );
+}
 //xivapi lodestone search
 var searchAPIFinished = false;
 var tempObj;
