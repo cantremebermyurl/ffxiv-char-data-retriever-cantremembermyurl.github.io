ea75056da953fcebc8f6a5fcf40655ace5669d09
Alex Hua <alexhua97@gmail.com>
2019-04-03 01:20:05 -0700

    Added equipment name/icons

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 //openweathermap api
-//var city = "Alameda";
 function getWeather(city){
   $.getJSON(
     "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",us&APPID=2df45a75e1b2ded6721a5c074a1142c4&units=imperial",
     function(data){
-      /*console.log(data);*/
-
       var icon =
         "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
       var temp = data.main.temp;
@@ -38,8 +35,8 @@ function search(){
           console.log("=> search api came up with: "+data.Results[0].Name);
         tempObj = data.Results[0];
         charID = tempObj.ID;
-          console.log("=> charID is now: " + charID);
-        debugSearch();
+          //console.log("=> charID is now: " + charID);
+          console.log("=> search() calling getCharData("+charID+");");
         getCharData(charID);
       } catch (e) {
         $(".searchingText").text("Search failed! Did you put in the correct Character Name and Server?");
@@ -47,52 +44,51 @@ function search(){
     })
 }
 
-function debugSearch(){
-  if (!searchAPIFinished) {
-    console.log("=> search() API call did not go through!");
-  }else {
-    console.log("=> search() ended successfully");
-  }
-}
 var charID = 0;
 //xivapi /Character data
 function getCharData(ID){
-    console.log("=> getCharData() loaded");
+    //console.log("=> getCharData() loaded");
   var currentJobID = 0;
-    console.log("=> searching character api for: " + ID);
-  $.getJSON(
-    "https://xivapi.com/Character/" + ID)
+    //console.log("=> pulling character api for: " + ID);
+  var url = "https://xivapi.com/Character/" + ID;
+  $.getJSON(url)
     .then(function(data){
-      var name = data.Character.Name;
-        console.log("=> char api successfully loaded: " + name);
-        $(".searchingText").append(".");
-      var portrait = data.Character.Portrait;
+      try {
+        var name = data.Character.Name;
+          //console.log("=> char api successfully loaded: " + name);
+          $(".searchingText").append(".");
+        var portrait = data.Character.Portrait;
 
-      var server = data.Character.Server;
-      currentJobID += data.Character.ActiveClassJob.ClassID;
-        console.log("=> currentJobID("+currentJobID+")");
-      var currentLvl = data.Character.ActiveClassJob.Level;
+        var server = data.Character.Server;
+        currentJobID += data.Character.ActiveClassJob.ClassID;
+          //console.log("=> currentJobID("+currentJobID+")");
+        var currentLvl = data.Character.ActiveClassJob.Level;
 
-      $(".portrait").attr("src", portrait);
-      var lodestone = "https://na.finalfantasyxiv.com/lodestone/character/" + ID + "/";
-      $(".portraitURL").attr("href", lodestone);
-      $(".name").text(name);
-      $(".server").text(server);
-      $(".lvl").text("Lv. "+currentLvl);
-        console.log("=> getCharData() calling getJobData()");
-      getJobData(currentJobID);
+        $(".portrait").attr("src", portrait);
+        var lodestone = "https://na.finalfantasyxiv.com/lodestone/character/" + ID + "/";
+        $(".portraitURL").attr("href", lodestone);
+        $(".name").text(name);
+        $(".server").text(server);
+        $(".lvl").text("Lv. "+currentLvl);
+          console.log("=> getCharData() calling getJobData(), getEquipment()");
+        getJobData(currentJobID);
+        getEquipment(url);
+      } catch (e) {
+        $(".searchingText").text("Something went wrong! Please try again.");
+      }
     })
+
 }
 
 //xivapi /ClassJob using data from /Character
 function getJobData(jobData){
-    console.log("=> getJobData("+jobData+") loaded");
+    //console.log("=> getJobData("+jobData+") loaded");
   $.getJSON(
     "https://xivapi.com/ClassJob/" + jobData)
     .then(function(data){
         $(".searchingText").append(".");
       var job = data.Name;
-        console.log("=> get API /ClassJob/"+jobData+" successful, job("+job+")");
+        //console.log("=> get API /ClassJob/"+jobData+" successful, job("+job+")");
       var jobIcoURL = "https://xivapi.com" + data.Icon;
       $(".job").text(" "+job);
       $(".jobIcon").attr("src", jobIcoURL);
@@ -100,19 +96,51 @@ function getJobData(jobData){
     })
 }
 
-//populate dropdown menu with xivapi.com/servers json list
-//https://www.codebyamir.com/blog/populate-a-select-dropdown-list-with-json <= base code
+function populateServerList(){
+  //populate dropdown menu with xivapi.com/servers json list
+  //https://www.codebyamir.com/blog/populate-a-select-dropdown-list-with-json <= base code
+  $("#serverList").append("<option selected=\"true\" disabled>Choose server</option>");
+  $("#serverList").prop("selectedIndex", 0);
+  var url = "https://xivapi.com/servers";
+  $("#serverList").append($("<option></option>").attr("value", "TestValue").text("TestText"));
+  // Populate dropdown with list of provinces
+  $.getJSON(url, function (data) {
+    $.each(data, function (key, entry) {
+      $("#serverList").append($("<option></option>").attr("value", entry).text(entry));
+    })
+  });
+  //set default server displayed to Goblin
+  $("#serverList").attr("selected", "Goblin");
+}
 
-$("#serverList").append("<option selected=\"true\" disabled>Choose server</option>");
-$("#serverList").prop("selectedIndex", 0);
-
-const url = "https://xivapi.com/servers";
-$("#serverList").append($("<option></option>").attr("value", "TestValue").text("TestText"));
-// Populate dropdown with list of provinces
-$.getJSON(url, function (data) {
-  $.each(data, function (key, entry) {
-    $("#serverList").append($("<option></option>").attr("value", entry).text(entry));
-  })
-});
-//set default server displayed to Goblin
-$("#serverList").attr("selected", "Goblin");
+//populate #equipment list
+function getEquipment(url){
+  //console.log("=> getEquipment("+url+")");
+  $(".leftColumn").empty();
+  $(".rightColumn").empty();
+  var obj;
+  $.getJSON(url,
+  function (data) {
+    obj = data.Character.GearSet.Gear;
+    //console.log(obj);
+    var gearName;
+    var gearIcon;
+    var n = 0;
+    $.each(obj, function (key, entry) {
+      //console.log(entry.ID);
+      $.getJSON("https://xivapi.com/item/"+entry.ID)
+      .then(function(data){
+        gearName = data.Name;
+        //console.log("Name:"+gearName);
+        gearIcon = data.Icon;
+        //console.log("Icon:"+gearIcon);
+        if (n < 7) {
+          $(".leftColumn").append($("<p>"+gearName+"     <img src=\"https://xivapi.com"+gearIcon+"\" alt=\""+gearName+"\" style=\"width:32px;height:32px;\"></p>"));
+          n++;
+        }else {
+          $(".rightColumn").append($("<p><img src=\"https://xivapi.com"+gearIcon+"\" alt=\""+gearName+"\" style=\"width:32px;height:32px;\">     "+gearName+"</p>"));
+        }
+      });
+    })
+  });
+}
